7353e0b244961ba98669e7e8bf03bf31d82cdee2
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-27 03:21:39 -0800

    Update product.rs

--- src-tauri/src/models/product.rs
+++ src-tauri/src/models/product.rs
@@ -5,7 +5,11 @@ use sqlx::FromRow;
 pub struct Product {
     pub id: i64,
     pub name: String,
-    pub category: String,
-    pub supplier: String,
     pub description: Option<String>,
+    pub category_id: i64,
+    pub supplier_id: i64,
+    pub price_purchase: f64,
+    pub price_sell: f64,
+    pub stock_initial: i64,
+    pub stock_current: i64,
 }
