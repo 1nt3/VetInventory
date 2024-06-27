f50b3a27a59a97b58f2038a173b542dc4c2830cd
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-27 03:31:28 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import LoginSignup from "./components/LoginSignup/LoginSignup";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Panel from "./components/Panel/Panel";
+import ProductEntry from "./components/RegisterProduct/ProductEntry";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginSignup />} />
+        {/* 
+          Comentado para probar la función (command) create_product que se encuentra en el componente ProductEntry (que es temporal).
+          <Route path="/" element={<LoginSignup />} />
+        */}
+        <Route path="/" element={<ProductEntry />} />
         <Route path="/panel" element={<Panel />} />
       </Routes>
     </Router>
