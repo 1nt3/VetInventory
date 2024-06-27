59342fdee8d27088f196f0c6e20924e9241df0a1
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-27 01:21:39 -0800

    Update login.rs

--- src-tauri/src/handlers/login.rs
+++ src-tauri/src/handlers/login.rs
@@ -1,30 +1,32 @@
 use crate::database::Database;
-use sqlx::query;
-use tauri::{command, State};
+use tauri::State;
 
-#[command]
+#[tauri::command]
 pub async fn is_credential_valid(
     state: State<'_, Database>,
     email: &str,
     password: &str,
 ) -> Result<bool, String> {
-    let query = query!(
-        r#"
-        SELECT EXISTS(SELECT 1 FROM user WHERE email = ? AND password = ?) AS "exists"
-        "#,
+    let mut pool_conn = match state.clone().get_connection().await {
+        Ok(conn) => conn,
+        Err(e) => return Err(format!("Failed to get database connection: {}", e)),
+    };
+
+    let query = sqlx::query!(
+        "SELECT email FROM user WHERE email = ? AND password = ?",
         email,
         password
     );
 
-    let result = query
-        .fetch_one(&state.pool)
-        .await
-        .map_err(|e| format!("Failed to execute query: {}", e))?;
-
-    let is_valid = result.exists.unwrap_or(0);
-    Ok(is_valid != 0)
+    match query.fetch_one(&mut *pool_conn).await {
+        Ok(result) => {
+            println!("{:?}", result);
+            Ok(true)
+        }
+        Err(sqlx::Error::RowNotFound) => Ok(false),
+        Err(e) => Err(format!("Failed to execute query: {}", e)),
+    }
 }
-
 //SELECT EXISTS(SELECT 1 FROM users WHERE email = ? AND password = ?);
 
 // SELECT EXISTS(SELECT 1 FROM users WHERE email = ? AND password = ?);
@@ -36,30 +38,5 @@ WHERE email = ? AND password = ?;
 */
 
 /*
-#[command]
-pub async fn create_product(
-    state: State<'_, Database>,
-    name: &str,
-    category: &str,
-    supplier: &str,
-    description: Option<String>,
-) -> Result<Product, String> {
-    let new_product = sqlx::query_as!(
-        Product,
-        r#"
-        INSERT INTO product (name, category, supplier, description)
-        VALUES (?, ?, ?, ?)
-        RETURNING id, name, category, supplier, description
-        "#,
-        name,
-        category,
-        supplier,
-        description
-    )
-    .fetch_one(&state.pool)
-    .await
-    .map_err(|e| format!("Failed to create product: {}", e))?;
-    println!("{:?}", new_product);
-    Ok(new_product)
-}
+
 */
