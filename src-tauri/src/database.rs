dc66a66037ce2b878561a07d752e3bc8fc49f5c5
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-27 01:21:15 -0800

    Update database.rs
    Add Arc and Mutex

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,31 +1,31 @@
 use std::str::FromStr;
+use std::sync::Arc;
 
 use sqlx::error::Error;
-use sqlx::migrate::Migrator;
+//use sqlx::migrate::Migrator;
 use sqlx::sqlite::SqliteConnectOptions;
 use sqlx::sqlite::SqlitePool;
+use tauri::async_runtime::Mutex;
+//use tokio::sync::Mutex;
 
-const DB_URL: &str = "sqlite://sqlite.db";
-static MIGRATOR: Migrator = sqlx::migrate!();
+const DB_URL: &str = "sqlite://database/sqlite.db";
 
 pub struct Database {
-    pub pool: SqlitePool,
+    pool: Arc<Mutex<SqlitePool>>,
 }
 
 impl Database {
     pub async fn connect() -> Result<Self, Error> {
-        let connect_options = SqliteConnectOptions::from_str(DB_URL)?.create_if_missing(true);
-        let pool = SqlitePool::connect_with(connect_options).await?;
-        Ok(Database { pool })
+        let connect_options = SqliteConnectOptions::from_str(DB_URL)?;
+        let pool_sqlite = SqlitePool::connect_with(connect_options).await?;
+
+        Ok(Database {
+            pool: Arc::new(Mutex::new(pool_sqlite)),
+        })
     }
 
-    pub async fn migrate(&self) -> Result<(), Error> {
-        MIGRATOR.run(&self.pool).await?;
-        Ok(())
+    pub async fn get_connection(&self) -> sqlx::Result<sqlx::pool::PoolConnection<sqlx::Sqlite>> {
+        let pool = self.pool.lock().await;
+        pool.acquire().await
     }
-
-    /*
-    pub fn get_pool(&self) -> &SqlitePool {
-        &self.pool
-    }*/
 }
