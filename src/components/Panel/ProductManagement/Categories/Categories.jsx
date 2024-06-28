433d2d03feee28fdaa012340cc3595f7112fc5a4
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-28 03:04:10 -0800

    Upload Categories

---
+++ src/components/Panel/ProductManagement/Categories/Categories.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./Categories.css";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    // Llamada a la función get_categoryos de Tauri
+    /*
+    invoke("get_productos")
+      .then((response) => {
+        setProductos(response);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los productos:", error);
+      });*/
+  }, []);
+  return (
+    <div className="table-container">
+      <h2 className="table-title">Categorías</h2>
+      <div className="actions">
+        <button className="add-button">Agregar</button>
+        <button className="edit-button">Editar</button>
+        <button className="delete-button">Eliminar</button>
+      </div>
+      <table className="categories-table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Cantidad de productos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category, index) => (
+            <tr key={index}>
+              <td>{category.nombre}</td>
+              <td>{category.cantidad_productos}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Categories;
