e13eaae3505d06c4c36da326601a12c30696adde
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-28 03:08:01 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import LoginSignup from "./components/LoginSignup/LoginSignup";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import LoginSignup from "./components/Login/LoginSignup/LoginSignup";
 import Panel from "./components/Panel/Panel";
-import Loading from "./components/LoginSignup/Loading";
-import LogoutLoading from "./components/LoginSignup/LogoutLoading"; // Importar el nuevo componente
+import Loading from "./components/Login/Loading/Loading";
+import LogoutLoading from "./components/Login/LogoutLoading/LogoutLoading"; // Importar el nuevo componente
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -11,10 +16,23 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginSignup setAuthenticated={setIsAuthenticated} />} />
+        <Route
+          path="/"
+          element={<LoginSignup setAuthenticated={setIsAuthenticated} />}
+        />
         <Route path="/loading" element={<Loading />} />
-        <Route path="/logout-loading" element={<LogoutLoading />} /> {/* Nueva ruta */}
-        <Route path="/panel" element={isAuthenticated ? <Panel setAuthenticated={setIsAuthenticated} /> : <Navigate to="/" />} />
+        <Route path="/logout-loading" element={<LogoutLoading />} />{" "}
+        {/* Nueva ruta */}
+        <Route
+          path="/panel"
+          element={
+            isAuthenticated ? (
+              <Panel setAuthenticated={setIsAuthenticated} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
       </Routes>
     </Router>
   );
