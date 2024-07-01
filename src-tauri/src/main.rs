7812b6ecf8399cbd9e657689c210ea65f3f89415
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-30 18:20:08 -0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,7 @@ mod repository;
 
 use database::Database;
 use handlers::login::is_credential_valid;
-use handlers::product_manager::{create_product, get_products};
+use handlers::product_manager::{create_product, get_categories, get_products, get_suppliers};
 
 use tauri::async_runtime::block_on;
 
@@ -19,7 +19,9 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             is_credential_valid,
             create_product,
-            get_products
+            get_products,
+            get_categories,
+            get_suppliers,
         ])
         .run(tauri::generate_context!())
         .expect("Failed to run Tauri application");
