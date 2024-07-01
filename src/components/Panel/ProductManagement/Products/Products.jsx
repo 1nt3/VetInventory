5cb8267498083db20308bb5be65257a4f65de9ed
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-30 19:24:19 -0800

    Update Products.jsx

--- src/components/Panel/ProductManagement/Products/Products.jsx
+++ src/components/Panel/ProductManagement/Products/Products.jsx
@@ -3,21 +3,19 @@ import { invoke } from "@tauri-apps/api/tauri";
 import Modal from "../Modal";
 import "./Products.css";
 
-const Products = () => {
+const Product = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [suppliers, setSuppliers] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [formValues, setFormValues] = useState({
     name: "",
     description: "",
     category_id: 0,
     supplier_id: 0,
   });
-  const [isDeleteMode, setIsDeleteMode] = useState(false);
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null); // Estado para el producto seleccionado
 
   useEffect(() => {
     fetchProducts();
@@ -53,24 +51,39 @@ const Products = () => {
   };
 
   const handleAddButtonClick = () => {
+    setIsAddModalOpen(true);
+  };
+
+  const handleEditButtonClick = (product) => {
+    setSelectedProduct(product);
+    setFormValues({
+      name: product.name,
+      description: product.description,
+      category_id: product.category_id,
+      supplier_id: product.supplier_id,
+    });
+    setIsEditModalOpen(true);
+  };
+
+  const handleCloseAddModal = () => {
+    setIsAddModalOpen(false);
     setFormValues({
       name: "",
       description: "",
       category_id: 0,
       supplier_id: 0,
     });
-    setIsModalOpen(true);
   };
 
-  const handleEditButtonClick = () => {
-    setIsEditMode(!isEditMode);
-    setSelectedProducts([]);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setIsEditMode(false);
-    setSelectedProducts([]);
+  const handleCloseEditModal = () => {
+    setIsEditModalOpen(false);
+    setSelectedProduct(null);
+    setFormValues({
+      name: "",
+      description: "",
+      category_id: 0,
+      supplier_id: 0,
+    });
   };
 
   const handleInputChange = (e) => {
@@ -78,36 +91,40 @@ const Products = () => {
     setFormValues({ ...formValues, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleAddSubmit = async (e) => {
     e.preventDefault();
     const { name, description, category_id, supplier_id } = formValues;
-    const categoryId = parseInt(category_id);
-    const supplierId = parseInt(supplier_id);
 
     try {
-      if (isEditMode) {
-        await invoke("update_product", {
-          id: selectedProducts[0],
-          name,
-          description,
-          categoryId,
-          supplierId,
-        });
-      } else {
-        await invoke("create_product", {
-          name,
-          description,
-          categoryId,
-          supplierId,
-        });
-      }
-      fetchProducts();
-      handleCloseModal();
+      await invoke("create_product", {
+        name,
+        description,
+        categoryId: parseInt(category_id),
+        supplierId: parseInt(supplier_id),
+      });
+      fetchProducts(); // Recargar la lista de productos después de agregar uno nuevo
+      handleCloseAddModal();
     } catch (error) {
-      console.error(
-        `Error al ${isEditMode ? "editar" : "agregar"} el producto:`,
-        error
-      );
+      console.error("Error al agregar el producto:", error);
+    }
+  };
+
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    const { name, description, category_id, supplier_id } = formValues;
+
+    try {
+      await invoke("update_product", {
+        id: selectedProduct.id,
+        name,
+        description,
+        categoryId: parseInt(category_id),
+        supplierId: parseInt(supplier_id),
+      });
+      fetchProducts(); // Recargar la lista de productos después de editar
+      handleCloseEditModal();
+    } catch (error) {
+      console.error("Error al editar el producto:", error);
     }
   };
 
@@ -121,125 +138,52 @@ const Products = () => {
     return supplier ? supplier.name : "";
   };
 
-  const handleDeleteButtonClick = () => {
-    setSelectedProducts([]);
-    setIsDeleteMode(!isDeleteMode);
-  };
-
-  const handleProductSelect = (productId) => {
-    if (isEditMode) {
-      const selectedProduct = products.find((p) => p.id === productId);
-      if (selectedProduct) {
-        setFormValues({
-          name: selectedProduct.name,
-          category_id: selectedProduct.category_id,
-          supplier_id: selectedProduct.supplier_id,
-          description: selectedProduct.description,
-        });
-        setSelectedProducts([productId]);
-        setIsModalOpen(true);
-      }
-    } else {
-      setSelectedProducts((prevSelected) =>
-        prevSelected.includes(productId)
-          ? prevSelected.filter((id) => id !== productId)
-          : [...prevSelected, productId]
-      );
-    }
-  };
-
-  const handleConfirmDeleteClick = () => {
-    if (selectedProducts.length > 0) {
-      setIsDeleteModalOpen(true);
-    }
-  };
-
-  const handleConfirmDelete = async () => {
-    try {
-      await invoke("delete_products", { productIds: selectedProducts });
-      fetchProducts();
-      setIsDeleteModalOpen(false);
-      setIsDeleteMode(false);
-      setSelectedProducts([]);
-    } catch (error) {
-      console.error("Error al eliminar los productos:", error);
-    }
-  };
-
   return (
-    <div className="products-container">
+    <div className="table-container">
       <h2 className="table-title">Productos</h2>
       <div className="actions">
-        {!isDeleteMode && !isEditMode && (
-          <button className="add-button" onClick={handleAddButtonClick}>
-            Agregar
-          </button>
-        )}
-        <button
-          className={isEditMode ? "cancel-edit-button" : "edit-button"}
-          onClick={handleEditButtonClick}
-        >
-          {isEditMode ? "Cancelar Edición" : "Editar"}
+        <button className="add-button" onClick={handleAddButtonClick}>
+          Agregar
         </button>
-        <button
-          className={isDeleteMode ? "cancel-delete-button" : "delete-button"}
-          onClick={handleDeleteButtonClick}
-        >
-          {isDeleteMode ? "Cancelar Eliminación" : "Eliminar"}
-        </button>
-        {isDeleteMode && (
-          <button
-            className="confirm-delete-button"
-            onClick={handleConfirmDeleteClick}
-            disabled={selectedProducts.length === 0}
-          >
-            Confirmar Eliminación
-          </button>
-        )}
       </div>
+
       <div className="table-wrapper">
         <table className="products-table">
           <thead>
             <tr>
-              {(isDeleteMode || isEditMode) && <th>Seleccionar</th>}
               <th>Nombre</th>
               <th>Categoría</th>
               <th>Proveedor</th>
               <th>Descripción</th>
+              <th>Acciones</th>
             </tr>
           </thead>
+
           <tbody>
-            {products.map((product) => (
-              <tr key={product.id}>
-                {(isDeleteMode || isEditMode) && (
-                  <td>
-                    <input
-                      type="checkbox"
-                      disabled={
-                        isEditMode &&
-                        selectedProducts.length === 1 &&
-                        !selectedProducts.includes(product.id)
-                      }
-                      onChange={() => handleProductSelect(product.id)}
-                      checked={selectedProducts.includes(product.id)}
-                    />
-                  </td>
-                )}
+            {products.map((product, index) => (
+              <tr key={index}>
                 <td>{product.name}</td>
                 <td>{getCategoryName(product.category_id)}</td>
                 <td>{getSupplierName(product.supplier_id)}</td>
                 <td>{product.description}</td>
+                <td>
+                  <button onClick={() => handleEditButtonClick(product)}>
+                    Editar
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
+      {/* Modal para agregar producto */}
       <Modal
-        title={isEditMode ? "Editar Producto" : "Agregar Producto"}
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
+        title="Agregar Producto"
+        isOpen={isAddModalOpen}
+        onClose={handleCloseAddModal}
       >
-        <form onSubmit={handleSubmit} className="product-form">
+        <form onSubmit={handleAddSubmit} className="product-form">
           <div className="form-group">
             <label>Nombre:</label>
             <input
@@ -293,34 +237,79 @@ const Products = () => {
           </div>
           <div className="form-actions">
             <button type="submit" className="add-product-button">
-              {isEditMode ? "Guardar Cambios" : "Agregar"}
+              Agregar
             </button>
           </div>
         </form>
       </Modal>
+
+      {/* Modal para editar producto */}
       <Modal
-        title="Confirmar Eliminación"
-        isOpen={isDeleteModalOpen}
-        onClose={() => setIsDeleteModalOpen(false)}
+        title="Editar Producto"
+        isOpen={isEditModalOpen}
+        onClose={handleCloseEditModal}
       >
-        <p>¿Está seguro de que desea eliminar los productos seleccionados?</p>
-        <div className="form-actions">
-          <button
-            onClick={() => setIsDeleteModalOpen(false)}
-            className="cancel-button"
-          >
-            Cancelar
-          </button>
-          <button
-            onClick={handleConfirmDelete}
-            className="confirm-delete-button"
-          >
-            Confirmar
-          </button>
-        </div>
+        <form onSubmit={handleEditSubmit} className="product-form">
+          <div className="form-group">
+            <label>Nombre:</label>
+            <input
+              type="text"
+              name="name"
+              value={formValues.name}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Categoría:</label>
+            <select
+              name="category_id"
+              value={formValues.category_id}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Seleccione una categoría</option>
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Proveedor:</label>
+            <select
+              name="supplier_id"
+              value={formValues.supplier_id}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Seleccione un proveedor</option>
+              {suppliers.map((supplier) => (
+                <option key={supplier.id} value={supplier.id}>
+                  {supplier.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Descripción:</label>
+            <textarea
+              name="description"
+              value={formValues.description}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-actions">
+            <button type="submit" className="add-product-button">
+              Guardar Cambios
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
 };
 
-export default Products;
+export default Product;
